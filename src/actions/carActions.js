60eeb94c9ffda61bffc8ef40d9c7aaf8b6bedda4
Daniel Mattox <dmattox10@gmail.com>
2020-01-22 16:32:34 -0500

    Housekeeping

--- src/actions/carActions.js
+++ src/actions/carActions.js
@@ -1,9 +1,7 @@
 export const removeFeature = id => {
-    console.log(id)
     return { type: 'REMOVE_FEATURE', payload: id }
 }
 
 export const buyItem = id => {
-    console.log(id)
     return { type: 'BUY_ITEM', payload: id }
 }
